30adc6cc2e2e852b92d539ebd1c359f830e30f7a
Aketh Gowda <aketh.tm@gmail.com>
2016-04-19 12:18:53 +0530

    correct in input, no zero as input when n is above 8

--- baroclinic.F90
+++ baroclinic.F90
@@ -620,13 +620,13 @@
 
    !dir$ offload begin target(mic:1)in(TRACER,UVEL,VVEL,this_block,hmix_tracer_itype,tavg_HDIFE_TRACER,tavg_HDIFN_TRACER,tavg_HDIFB_TRACER) &
    !dir$ in(lsubmesoscale_mixing,dt,dtu,HYX,HXY,RZ_SAVE,RX,RY,TX,TY,TZ,KMT,KMTE,KMTN,implicit_vertical_mix,vmix_itype,KPP_HBLT,HMXL) &
-   !dir$ in(HYXW,HXYS,UIT,VIT,RB,RBR,BL_DEPTH,mixtime,nblocks_clinic,blocks_clinic,nblocks_tot,curtime) &
+   !dir$ in(HYXW,HXYS,UIT,VIT,RB,RBR,BL_DEPTH,mixtime,nblocks_clinic,nblocks_tot,curtime) &
    !dir$ in(kappa_isop_type,kappa_thic_type, kappa_freq,slope_control,SLA_SAVE,nsteps_total, ah,ah_bolus, ah_bkg_bottom,ah_bkg_srfbl) &
    !dir$ in(slm_r,slm_b,compute_kappa,BUOY_FREQ_SQ,SIGMA_TOPO_MASK,dz,dzw,dzwr,zw,dzr,DYT,DXT,HUW,HUS,TAREA_R,HTN,HTE,pi,zt) &
    !dir$ in(luse_const_horiz_len_scale,hor_length_scale,TIME_SCALE,efficiency_factor,TLT,my_task,master_task) & 
    !dir$ in(max_hor_grid_scale,mix_pass,grav,zgrid,DZT,partial_bottom_cells,FCORT,linertial,ldiag_cfl,radian,TLAT,eod_last,all_blocks) &
    !dir$ in(ltavg_on,num_avail_tavg_fields,sigo,state_coeffs,to,so,use_const_ah_bkg_srfbl,transition_layer_on,tavg_HDIFS,tavg_HDIFT) &
-   !dir$ out(WORKN_PHI:alloc_if(.false.) free_if(.false.)) inout(VDC,VDC_GM) &
+   !dir$ out(WORKN_PHI:alloc_if(.false.) free_if(.false.)) inout(VDC,VDC_GM) in(blocks_clinic : LENGTH(16) alloc_if(.true.) free_if(.false.) )&
    !dir$ nocopy(SLX,SLY,SF_SUBM_X,SF_SUBM_Y,KAPPA_ISOP,KAPPA_THIC,HOR_DIFF,KAPPA_VERTICAL,KAPPA_LATERAL,SF_SLX,SF_SLY,WTOP_ISOP: alloc_if(.false.) free_if(.false.) ) &
    !dir$ nocopy( WBOT_ISOP : alloc_if(.false.) free_if(.false.) )
 
@@ -636,10 +636,10 @@
    
   do iblock = 1,nblocks_clinic
       this_block = get_block(blocks_clinic(iblock),iblock)
-      !print *,"working on ID",thread_id 
+      print *,"input is",blocks_clinic(iblock),iblock
       do k = 1,km
 
-      call hdifft(k, WORKN_PHI(:,:,:,k,thread_id),TRACER (:,:,:,:,mixtime,iblock),UVEL(:,:,:,mixtime,iblock), VVEL(:,:,:,mixtime,iblock), this_block)
+      call hdifft(k, WORKN_PHI(:,:,:,k,iblock),TRACER (:,:,:,:,mixtime,iblock),UVEL(:,:,:,mixtime,iblock), VVEL(:,:,:,mixtime,iblock), this_block)
 
       enddo
       thread_id = thread_id + 1
